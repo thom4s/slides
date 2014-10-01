c43cce8729f2913394472482257dde7c5e5abb77
thom4s <thomasflorentin@yahoo.fr>
2014-10-01 16:26:24 +0200

    Modify slide plugin for SuperSlide.js

--- index.php
+++ index.php
@@ -9,31 +9,33 @@ $dirlist = getFileList("img");
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="superslides.css">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-	<script type="text/javascrip" src="vendors/responsiveslides.min.js"></script>
+	<script type="text/javascript" src="vendors/jquery.superslides.js"></script>
 	<script type="text/javascript" src="script.js"></script>
 </head>
 <body>
 
-<ul class="rslides">
-	
-<?php foreach($dirlist as $file): ?>
-	<?php if(!preg_match("/\.jpg$/", $file['name'])) continue; ?>
 
-		<li>
-			<img src="<?php echo $file['name']; ?>">
-		</li>
+	<div id="slides">
+	  	<ul class="slides-container">
+		
+			<?php foreach($dirlist as $file): ?>
+				<?php if(!preg_match("/\.jpg$/", $file['name'])) continue; ?>
 
-<?php endforeach; ?>
-
-
-</ul>
+					<li>
+						<img src="<?php echo $file['name']; ?>">
+					</li>
 
 
+			<?php endforeach; ?>
 
-
-
-
+		</ul>
+		<nav class="slides-navigation">
+			<a href="#" class="next">Next</a>
+		    <a href="#" class="prev">Previous</a>
+		</nav>
+	</div>
 
 </body>
 </html>
