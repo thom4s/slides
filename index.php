50173ae142239da11e3dc46604ddf23d3efb1381
thom4s <thomasflorentin@yahoo.fr>
2014-10-01 15:50:36 +0200

    Add ResponsiveSlide.js

--- index.php
+++ index.php
@@ -1,26 +1,40 @@
 <?php
 
 require "parser.php";
-
 $dirlist = getFileList("img");
 
-// output file list as HTML table 
-echo "<table border=\"1\">\n"; echo "<thead>\n"; 
-echo "<tr><th></th><th>Name</th><th>Type</th><th>Size</th><th>Last Modified</th></tr>\n"; 
-echo "</thead>\n"; 
-echo "<tbody>\n";
-
-foreach($dirlist as $file) { 
-	if(!preg_match("/\.jpg$/", $file['name'])) continue; 
-	echo "<tr>\n"; 
-	echo "<td><img src=\"{$file['name']}\" width=\"64\"></td>\n"; 
-	echo "<td>{$file['name']}</td>\n"; echo "<td>{$file['type']}</td>\n"; 
-	echo "<td>{$file['size']}</td>\n"; 
-	echo "<td>",date('r', $file['lastmod']),"</td>\n"; 
-	echo "</tr>\n"; 
-} 
-echo "</tbody>\n";
-echo "</table>\n\n";
-
-
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script type="text/javascrip" src="vendors/responsiveslides.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+</head>
+<body>
+
+<ul class="rslides">
+	
+<?php foreach($dirlist as $file): ?>
+	<?php if(!preg_match("/\.jpg$/", $file['name'])) continue; ?>
+
+		<li>
+			<img src="<?php echo $file['name']; ?>">
+		</li>
+
+<?php endforeach; ?>
+
+
+</ul>
+
+
+
+
+
+
+
+</body>
+</html>
+
